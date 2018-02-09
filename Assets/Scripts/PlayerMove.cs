09b0f8a48b2aba9f21e32d82127196e80bc5d5f2
ChiefTripodBear <avalacos@gmail.com>
2018-02-09 22:54:43 +1100

    commit 1
    syncing local and online repos

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float turnSpeed;
+    public float moveSpeed;
+    public float speedGauge;
+
+    public Rigidbody rb;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+	    
+	void Update ()
+    {
+
+        checkTurn();
+
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            if (speedGauge >= 0)
+            {
+                speedGauge -= 1f;
+                //increase/decrease velocity by multiplying getaxis.vertical by time.deltatime
+            } 
+            
+        }
+        else
+        {
+            //set velocity to the standard bullet speed
+        }
+
+    }
+
+    private void checkTurn()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            transform.Rotate(0f, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0f);
+        }
+    }
+}
